0b539b6a0db33616f53c07adfb72e139dd483010
SivanuHappy <anusha25cse@gmail.com>
2018-05-23 10:28:13 -0500

    Final application

--- src/com/jobapplication/test/config/TestBeanConfig.java
+++
@@ -1,9 +0,0 @@
-package com.jobapplication.test.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@ComponentScan(basePackages = "com.developerstack")
-public class TestBeanConfig {
-
-}
